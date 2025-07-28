f831e469d8842d403abd696acd5bdcf2223ef6ba
sguschin <sguschin@gear-games.com>
2025-07-29 01:03:27 +0400

    Recursion: Форматирование

--- RecursionTasks.cs
+++ RecursionTasks.cs
@@ -4,23 +4,22 @@ namespace Recursion
 {
     public static class RecursionTasks
     {
-        //1. dозведение числа N в степень M;
+        //1. Возведение числа N в степень M;
         public static long RecursivePower(long number, uint power)
         {
             long ZERO_POWER_RESULT = 1;
             return power > 0 ? number * RecursivePower(number, power - 1) : ZERO_POWER_RESULT;
         }
         
-        //2. cумма цифр числа;
+        //2. Вычисление суммы цифр числа;
         public static long SumOfDigits(long number)
         {
             return number > 0 ? number % 10 + SumOfDigits(number / 10) : 0;
         }
         
-        //3. расчёт длины списка, для которого разрешена только операция удаления первого элемента pop(0) (и получение длины конечно);
+        //3. Расчёт длины списка, для которого разрешена только операция удаления первого элемента pop(0) (и получение длины конечно);
         public static int LengthOfListRecursive(List<int> list)
         {
-            // Если допустимо сравнивать с 1, то получится на 1 итерацию меньше. 
             if (list.Count > 0)
             {
                 list.RemoveAt(0);
@@ -30,7 +29,7 @@ namespace Recursion
             return 0;
         }
         
-        //4. проверка, является ли строка палиндромом;
+        //4. Проверка, является ли строка палиндромом;
         public static bool IsPalindromeRecursive(string str)
         {
             if (str.Length <= 1)
