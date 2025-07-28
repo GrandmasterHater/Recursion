cdd1eb02e2e80e0c9882e180f8dfb8763653e806
sguschin <sguschin@gear-games.com>
2025-07-29 00:57:42 +0400

    Recursion: Tasks 1, 2, 3, 4

---
+++ RecursionTasks.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace Recursion
+{
+    public static class RecursionTasks
+    {
+        //1. dозведение числа N в степень M;
+        public static long RecursivePower(long number, uint power)
+        {
+            long ZERO_POWER_RESULT = 1;
+            return power > 0 ? number * RecursivePower(number, power - 1) : ZERO_POWER_RESULT;
+        }
+        
+        //2. cумма цифр числа;
+        public static long SumOfDigits(long number)
+        {
+            return number > 0 ? number % 10 + SumOfDigits(number / 10) : 0;
+        }
+        
+        //3. расчёт длины списка, для которого разрешена только операция удаления первого элемента pop(0) (и получение длины конечно);
+        public static int LengthOfListRecursive(List<int> list)
+        {
+            // Если допустимо сравнивать с 1, то получится на 1 итерацию меньше. 
+            if (list.Count > 0)
+            {
+                list.RemoveAt(0);
+                return 1 + LengthOfListRecursive(list);
+            }
+            
+            return 0;
+        }
+        
+        //4. проверка, является ли строка палиндромом;
+        public static bool IsPalindromeRecursive(string str)
+        {
+            if (str.Length <= 1)
+                return true;
+            
+            char firstChar = str[0];
+            char lastChar = str[str.Length - 1];
+            bool isFirstCharLetterOrDigit = char.IsLetterOrDigit(firstChar);
+            bool isLastCharLetterOrDigit = char.IsLetterOrDigit(lastChar);
+            
+            if (isFirstCharLetterOrDigit && isLastCharLetterOrDigit)
+            {
+                return char.ToLower(firstChar) == char.ToLower(lastChar) && IsPalindromeRecursive(str.Substring(1, str.Length - 2));
+            }
+            
+            int startIndex = isFirstCharLetterOrDigit ? 0 : 1;
+            int length = isFirstCharLetterOrDigit ? str.Length : str.Length - 1;
+            length = isLastCharLetterOrDigit ? length : length - 1;
+
+            return IsPalindromeRecursive(str.Substring(startIndex, length));
+        }
+    }
+}
---
+++ RecursionTasksTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace Recursion
+{
+    [TestFixture]
+    public class RecursionTasksTests
+    {
+        [TestCase(2L, 3U, 8L)]
+        [TestCase(3L, 3U, 27L)]
+        public void RecursivePowerTest(long number, uint power, long expected)
+        {
+            Assert.That(RecursionTasks.RecursivePower(number, power), Is.EqualTo(expected));
+        }
+        
+        [TestCase(123, 6)]
+        [TestCase(777, 21)]
+        public void SumOfDigitsTest(long number, long expected)
+        {
+            Assert.That(RecursionTasks.SumOfDigits(number), Is.EqualTo(expected));
+        }
+
+        [TestCase(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, 9)]
+        [TestCase(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10)]
+        public void LengthOfListRecursiveTest(int[] array, int expectedCount)
+        {
+            Assert.That(RecursionTasks.LengthOfListRecursive(array.ToList()), Is.EqualTo(expectedCount));
+        }
+        
+        [TestCase("А роза упала на лапу Азора.", true)]
+        [TestCase("A man, a plan, a canal, Panama!", true)]
+        [TestCase("Сегодня отличная погода.", false)]
+        [TestCase("The sun is shining brightly.", false)]
+        public void IsPalindromeRecursiveTest(string str, bool expected)
+        {
+            Assert.That(RecursionTasks.IsPalindromeRecursive(str), Is.EqualTo(expected));
+        }
+    }
+}
