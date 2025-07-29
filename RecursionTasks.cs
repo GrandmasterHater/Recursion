100bbe5f978576629ca688d601020502f49d912c
sguschin <sguschin@gear-games.com>
2025-07-30 00:56:07 +0400

    Recursion: Fixes for tasks 1, 2, 3, 4. Complete tasks 5, 6, 7.

--- RecursionTasks.cs
+++ RecursionTasks.cs
@@ -1,55 +1,142 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace Recursion
 {
     public static class RecursionTasks
     {
         //1. Возведение числа N в степень M;
-        public static long RecursivePower(long number, uint power)
+        public static long Power(long number, uint power)
         {
-            long ZERO_POWER_RESULT = 1;
-            return power > 0 ? number * RecursivePower(number, power - 1) : ZERO_POWER_RESULT;
+            return RecursivePower(1, number, power);
+        }
+
+        private static long RecursivePower(long currentNumber, long number, uint power)
+        {
+            if (power == 0)
+                return currentNumber;
+            
+            return RecursivePower(currentNumber * number, number, power - 1);
         }
         
         //2. Вычисление суммы цифр числа;
         public static long SumOfDigits(long number)
         {
-            return number > 0 ? number % 10 + SumOfDigits(number / 10) : 0;
+            return SumOfDigitsRecursive(0, number);
+        }
+        
+        private static long SumOfDigitsRecursive(long numbersSum, long number)
+        {
+            if (number == 0)
+                return numbersSum;
+            
+            return SumOfDigitsRecursive(numbersSum + (number % 10), number / 10);
         }
         
         //3. Расчёт длины списка, для которого разрешена только операция удаления первого элемента pop(0) (и получение длины конечно);
-        public static int LengthOfListRecursive(List<int> list)
+        public static int LengthOfList(List<int> list)
         {
-            if (list.Count > 0)
-            {
-                list.RemoveAt(0);
-                return 1 + LengthOfListRecursive(list);
-            }
+            return LengthOfListRecursive(list, 0);
+        }
+        
+        private static int LengthOfListRecursive(List<int> list, int count)
+        {
+            if (list.Count == 0)
+                return count;
             
-            return 0;
+            list.RemoveAt(0);
+            return LengthOfListRecursive(list, ++count);
         }
         
         //4. Проверка, является ли строка палиндромом;
-        public static bool IsPalindromeRecursive(string str)
+        public static bool IsPalindrome(string str)
         {
-            if (str.Length <= 1)
+            return IsPalindromeRecursive(str, 0, str.Length - 1);
+        }
+
+        private static bool IsPalindromeRecursive(string str, int firstIndex, int lastIndex)
+        {
+            if (firstIndex >= lastIndex)
                 return true;
             
-            char firstChar = str[0];
-            char lastChar = str[str.Length - 1];
+            char firstChar = str[firstIndex];
+            char lastChar = str[lastIndex];
             bool isFirstCharLetterOrDigit = char.IsLetterOrDigit(firstChar);
             bool isLastCharLetterOrDigit = char.IsLetterOrDigit(lastChar);
             
             if (isFirstCharLetterOrDigit && isLastCharLetterOrDigit)
             {
-                return char.ToLower(firstChar) == char.ToLower(lastChar) && IsPalindromeRecursive(str.Substring(1, str.Length - 2));
+                return char.ToLower(firstChar) == char.ToLower(lastChar) && IsPalindromeRecursive(str, ++firstIndex, --lastIndex);
             }
             
-            int startIndex = isFirstCharLetterOrDigit ? 0 : 1;
-            int length = isFirstCharLetterOrDigit ? str.Length : str.Length - 1;
-            length = isLastCharLetterOrDigit ? length : length - 1;
+            firstIndex = isFirstCharLetterOrDigit ? firstIndex : ++firstIndex;
+            lastIndex = isLastCharLetterOrDigit ? lastIndex : --lastIndex;
 
-            return IsPalindromeRecursive(str.Substring(startIndex, length));
+            return IsPalindromeRecursive(str, firstIndex, lastIndex);
+        }
+        
+        //5. печать только чётных значений из списка;
+        public static List<int> PrintOnlyEvenNumbers(List<int> list)
+        {
+            List<int> result = new List<int>();
+            PrintOnlyEvenNumbersRecursive(list, 0, result);
+            return result;
+        }
+        
+        public static void PrintOnlyEvenNumbersRecursive(List<int> list, int currentIndex, List<int> result)
+        {
+            if (currentIndex >= list.Count)
+                return;
+            
+            if (list[currentIndex] % 2 == 0)
+                result.Add(list[currentIndex]);
+            
+            PrintOnlyEvenNumbersRecursive(list, ++currentIndex, result);
+        }
+        
+        //6. печать элементов списка с чётными индексами;
+        public static List<int> PrintOnlyEvenIndexes(List<int> list)
+        {
+            List<int> result = new List<int>();
+            PrintOnlyEvenIndexesRecursive(list, 0, result);
+            return result;
+        }
+        
+        public static void PrintOnlyEvenIndexesRecursive(List<int> list, int currentIndex, List<int> result)
+        {
+            if (currentIndex >= list.Count)
+                return;
+            
+            if (currentIndex % 2 == 0)
+                result.Add(list[currentIndex]);
+            
+            PrintOnlyEvenIndexesRecursive(list, ++currentIndex, result);
+        }
+        
+        //7. нахождение второго максимального числа в списке;
+        public static int FindSecondMax(List<int> list)
+        {
+            return FindSecondMaxRecursive(list, 0, int.MinValue, int.MinValue);
+        }
+        
+        private static int FindSecondMaxRecursive(List<int> list, int currentIndex, int max, int secondMax)
+        {
+            if (currentIndex >= list.Count)
+                return secondMax;
+            
+            int currentNumber = list[currentIndex];
+
+            if (currentNumber > max)
+            {
+                secondMax = max;
+                max = currentNumber;
+            }
+            else if (currentNumber > secondMax)
+            {
+                secondMax = currentNumber;
+            }
+                
+            return FindSecondMaxRecursive(list, ++currentIndex, max, secondMax);
         }
     }
 }
--- RecursionTasksTests.cs
+++ RecursionTasksTests.cs
@@ -10,7 +10,7 @@ namespace Recursion
         [TestCase(3L, 3U, 27L)]
         public void RecursivePowerTest(long number, uint power, long expected)
         {
-            Assert.That(RecursionTasks.RecursivePower(number, power), Is.EqualTo(expected));
+            Assert.That(RecursionTasks.Power(number, power), Is.EqualTo(expected));
         }
         
         [TestCase(123, 6)]
@@ -24,7 +24,7 @@ namespace Recursion
         [TestCase(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10)]
         public void LengthOfListRecursiveTest(int[] array, int expectedCount)
         {
-            Assert.That(RecursionTasks.LengthOfListRecursive(array.ToList()), Is.EqualTo(expectedCount));
+            Assert.That(RecursionTasks.LengthOfList(array.ToList()), Is.EqualTo(expectedCount));
         }
         
         [TestCase("А роза упала на лапу Азора.", true)]
@@ -33,7 +33,33 @@ namespace Recursion
         [TestCase("The sun is shining brightly.", false)]
         public void IsPalindromeRecursiveTest(string str, bool expected)
         {
-            Assert.That(RecursionTasks.IsPalindromeRecursive(str), Is.EqualTo(expected));
+            Assert.That(RecursionTasks.IsPalindrome(str), Is.EqualTo(expected));
+        }
+        
+        [TestCase(new int[] {1, 2, 3, 4, 5, 6}, new int[] {2, 4, 6})]
+        [TestCase(new int[] {2, 4, 6, 8, 10, 12}, new int[] {2, 4, 6, 8, 10, 12})]
+        [TestCase(new int[] {1, 3, 5, 7, 9}, new int[] {})]
+        public void PrintOnlyEvenNumbersRecursiveTest(int[] dataForList, int[] expected)
+        {
+            Assert.That(RecursionTasks.PrintOnlyEvenNumbers(dataForList.ToList()), Is.EqualTo(expected.ToList()));
+        }
+        
+        [TestCase(new int[] {1, 2, 3, 4, 5, 6}, new int[] {1, 3, 5})]
+        [TestCase(new int[] {2, 4, 6, 8, 10, 12}, new int[] {2, 6, 10})]
+        public void PrintOnlyEvenIndexesRecursiveTest(int[] dataForList, int[] expected)
+        {
+            Assert.That(RecursionTasks.PrintOnlyEvenIndexes(dataForList.ToList()), Is.EqualTo(expected.ToList()));
+        }
+
+        [TestCase(new int[] {1, 2, 3, 4, 5, 6}, 5)]
+        [TestCase(new int[] {3, 2, 7, 4, 5, 10}, 7)]
+        [TestCase(new int[] {8, 8, 7, 5, 3, 1}, 8)]
+        [TestCase(new int[] {1, 3, 5, 7, 8, 8}, 8)]
+        [TestCase(new int[] {1, 2}, 1)]
+        [TestCase(new int[] {2,1}, 1)]
+        public void FindSecondMaxTest(int[] dataForList, int expected)
+        {
+            Assert.That(RecursionTasks.FindSecondMax(dataForList.ToList()), Is.EqualTo(expected));
         }
     }
 }
