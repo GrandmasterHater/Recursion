8ad118307a3ecbe7d7060ed3aa310136e8f794f3
sguschin <sguschin@gear-games.com>
2025-07-31 10:30:41 +0400

    Recursion: Внесены правки в задачи 1-7, сделана задача 8 и доп. задание.

--- RecursionTasks.cs
+++ RecursionTasks.cs
@@ -1,78 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Recursion
 {
     public static class RecursionTasks
     {
         //1. Возведение числа N в степень M;
-        public static long Power(long number, uint power)
-        {
-            return RecursivePower(1, number, power);
-        }
-
-        private static long RecursivePower(long currentNumber, long number, uint power)
+        public static long RecursivePower(long number, uint power, long currentNumber = 1)
         {
             if (power == 0)
                 return currentNumber;
             
-            return RecursivePower(currentNumber * number, number, power - 1);
+            return RecursivePower(number, power - 1, currentNumber * number);
         }
         
         //2. Вычисление суммы цифр числа;
-        public static long SumOfDigits(long number)
-        {
-            return SumOfDigitsRecursive(0, number);
-        }
-        
-        private static long SumOfDigitsRecursive(long numbersSum, long number)
+        public static long SumOfDigitsRecursive(long number, long numbersSum = 0)
         {
             if (number == 0)
                 return numbersSum;
             
-            return SumOfDigitsRecursive(numbersSum + (number % 10), number / 10);
+            return SumOfDigitsRecursive(number / 10, numbersSum + (number % 10));
         }
         
         //3. Расчёт длины списка, для которого разрешена только операция удаления первого элемента pop(0) (и получение длины конечно);
-        public static int LengthOfList(List<int> list)
-        {
-            return LengthOfListRecursive(list, 0);
-        }
-        
-        private static int LengthOfListRecursive(List<int> list, int count)
+        public static int LengthOfListRecursive(List<int> list, int count = 0)
         {
             if (list.Count == 0)
                 return count;
             
             list.RemoveAt(0);
+            
             return LengthOfListRecursive(list, ++count);
         }
         
         //4. Проверка, является ли строка палиндромом;
         public static bool IsPalindrome(string str)
         {
-            return IsPalindromeRecursive(str, 0, str.Length - 1);
+            string filteredString = Regex.Replace(str, "[^a-zA-Zа-яА-Я0-9]", "").ToLower();
+            return IsPalindromeRecursive(filteredString, 0);
         }
-
-        private static bool IsPalindromeRecursive(string str, int firstIndex, int lastIndex)
+        
+        private static bool IsPalindromeRecursive(string str, int index)
         {
-            if (firstIndex >= lastIndex)
+            int lastIndex = str.Length - 1 - index;
+
+            if (index >= lastIndex)
                 return true;
             
-            char firstChar = str[firstIndex];
-            char lastChar = str[lastIndex];
-            bool isFirstCharLetterOrDigit = char.IsLetterOrDigit(firstChar);
-            bool isLastCharLetterOrDigit = char.IsLetterOrDigit(lastChar);
-            
-            if (isFirstCharLetterOrDigit && isLastCharLetterOrDigit)
-            {
-                return char.ToLower(firstChar) == char.ToLower(lastChar) && IsPalindromeRecursive(str, ++firstIndex, --lastIndex);
-            }
-            
-            firstIndex = isFirstCharLetterOrDigit ? firstIndex : ++firstIndex;
-            lastIndex = isLastCharLetterOrDigit ? lastIndex : --lastIndex;
+            if (str[index] != str[lastIndex])
+                return false;
 
-            return IsPalindromeRecursive(str, firstIndex, lastIndex);
+            return IsPalindromeRecursive(str, ++index);
+        }
+        
+        public static bool IsPalindromeWithLinkedList(string str)
+        {
+            LinkedList<char> lettersAndDigits = new LinkedList<char>();
+            FilterLowerLetterAndDigitsRecursive(str, lettersAndDigits, 0);
+            return IsPalindromeRecursive(lettersAndDigits);
+        }
+        
+        private static void FilterLowerLetterAndDigitsRecursive(string str, LinkedList<char> lettersAndDigits, int strIndex)
+        {
+            if (strIndex >= str.Length)
+                return;
+
+            if (char.IsLetterOrDigit(str[strIndex]))
+                lettersAndDigits.AddLast(char.ToLower(str[strIndex]));
+            
+            FilterLowerLetterAndDigitsRecursive(str, lettersAndDigits, ++strIndex);
+        }
+
+        private static bool IsPalindromeRecursive(LinkedList<char> str)
+        {
+            if (str.Count <= 1)
+                return true;
+
+            if (str.First.Value != str.Last.Value)
+                return false;
+            
+            str.RemoveFirst();
+            str.RemoveLast();
+
+            return IsPalindromeRecursive(str);
         }
         
         //5. печать только чётных значений из списка;
@@ -102,41 +117,126 @@ namespace Recursion
             return result;
         }
         
-        public static void PrintOnlyEvenIndexesRecursive(List<int> list, int currentIndex, List<int> result)
+        private static void PrintOnlyEvenIndexesRecursive(List<int> list, int currentIndex, List<int> result)
         {
+            const int EVEN_INDEX_STEP = 2;
+            
             if (currentIndex >= list.Count)
                 return;
             
-            if (currentIndex % 2 == 0)
-                result.Add(list[currentIndex]);
+            result.Add(list[currentIndex]);
             
-            PrintOnlyEvenIndexesRecursive(list, ++currentIndex, result);
+            PrintOnlyEvenIndexesRecursive(list, currentIndex + EVEN_INDEX_STEP, result);
         }
         
         //7. нахождение второго максимального числа в списке;
-        public static int FindSecondMax(List<int> list)
+        public static T FindSecondMax<T>(List<T> list) where T : IComparable
         {
-            return FindSecondMaxRecursive(list, 0, int.MinValue, int.MinValue);
+            T max = default;
+            T secondMax = default;
+            
+            if (list[0].CompareTo(list[1]) > 0)
+            {
+                max = list[0];
+                secondMax = list[1];
+            }
+            else
+            {
+                max = list[1];
+                secondMax = list[0];
+            }
+            
+            const int START_INDEX = 2;
+            
+            return FindSecondMaxRecursive(list, START_INDEX, max, secondMax);
         }
         
-        private static int FindSecondMaxRecursive(List<int> list, int currentIndex, int max, int secondMax)
+        private static T FindSecondMaxRecursive<T>(List<T> list, int currentIndex, T max, T secondMax) where T : IComparable
         {
             if (currentIndex >= list.Count)
                 return secondMax;
             
-            int currentNumber = list[currentIndex];
+            T currentNumber = list[currentIndex];
 
-            if (currentNumber > max)
+            if (currentNumber.CompareTo(max) > 0)
             {
                 secondMax = max;
                 max = currentNumber;
             }
-            else if (currentNumber > secondMax)
+            else if (currentNumber.CompareTo(secondMax) > 0)
             {
                 secondMax = currentNumber;
             }
                 
             return FindSecondMaxRecursive(list, ++currentIndex, max, secondMax);
+        }
+        
+        //8. поиск всех файлов в заданном каталоге, включая файлы, расположенные в подкаталогах произвольной вложенности.
+        public static List<FileInfo> FindAllFiles(string path)
+        {
+            List<FileInfo> foundFiles = new List<FileInfo>();
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+
+            FindAllFilesRecursive(directoryInfo, foundFiles);
+
+            return foundFiles;
+        }
+        
+        private static void FindAllFilesRecursive(DirectoryInfo directoryInfo, List<FileInfo> foundFiles)
+        {
+            foundFiles.AddRange(directoryInfo.GetFiles());
+
+            DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
+            
+            if (subDirectories.Length == 0)
+                return;
+            
+            FindFromDirectoriesRecursive(subDirectories, 0, foundFiles);
+        }
+
+        private static void FindFromDirectoriesRecursive(DirectoryInfo[] directoryInfo, int index, List<FileInfo> foundFiles)
+        {
+            if (index >= directoryInfo.Length)
+                return;
+            
+            FindAllFilesRecursive(directoryInfo[index], foundFiles);
+            FindFromDirectoriesRecursive(directoryInfo, ++index, foundFiles);
+        }
+        
+        //Генерация всех корректных сбалансированных комбинаций круглых скобок (параметр -- количество открывающих скобок).
+        public static string[] GenerateBalancedBrackets(int countOpeningBrackets)
+        {
+            StringBuilder initialCombination = new StringBuilder("(");
+            List<StringBuilder> combinations = new List<StringBuilder>() {initialCombination};
+            GenerateBalancedBracketsRecursive(initialCombination, combinations, countOpeningBrackets - 1, countOpeningBrackets);
+            return combinations.Select(sb => sb.ToString()).ToArray();
+        }
+
+        private static void GenerateBalancedBracketsRecursive(StringBuilder combination, List<StringBuilder> combinations, int countOpeningBrackets, int countClosingBrackets)
+        {
+            if (countOpeningBrackets == 0 && countClosingBrackets == 0)
+                return;
+            
+            if (countOpeningBrackets > 0 && countOpeningBrackets < countClosingBrackets)
+            {
+                StringBuilder newCombination = new StringBuilder(combination.ToString());
+                combination.Append('(');
+                GenerateBalancedBracketsRecursive(combination, combinations, countOpeningBrackets - 1, countClosingBrackets);
+                
+                newCombination.Append(")");
+                combinations.Add(newCombination);
+                GenerateBalancedBracketsRecursive(newCombination, combinations, countOpeningBrackets, countClosingBrackets - 1);
+            }
+            else if (countOpeningBrackets > 0 && countOpeningBrackets == countClosingBrackets)
+            {
+                combination.Append("(");
+                GenerateBalancedBracketsRecursive(combination, combinations, countOpeningBrackets - 1, countClosingBrackets);
+            }
+            else
+            {
+                combination.Append(")");
+                GenerateBalancedBracketsRecursive(combination, combinations, countOpeningBrackets, countClosingBrackets - 1);
+            }
         }
     }
 }
--- RecursionTasksTests.cs
+++ RecursionTasksTests.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using NUnit.Framework;
 
 namespace Recursion
@@ -10,21 +13,21 @@ namespace Recursion
         [TestCase(3L, 3U, 27L)]
         public void RecursivePowerTest(long number, uint power, long expected)
         {
-            Assert.That(RecursionTasks.Power(number, power), Is.EqualTo(expected));
+            Assert.That(RecursionTasks.RecursivePower(number, power), Is.EqualTo(expected));
         }
         
         [TestCase(123, 6)]
         [TestCase(777, 21)]
         public void SumOfDigitsTest(long number, long expected)
         {
-            Assert.That(RecursionTasks.SumOfDigits(number), Is.EqualTo(expected));
+            Assert.That(RecursionTasks.SumOfDigitsRecursive(number), Is.EqualTo(expected));
         }
-
+        
         [TestCase(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9}, 9)]
         [TestCase(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, 10)]
         public void LengthOfListRecursiveTest(int[] array, int expectedCount)
         {
-            Assert.That(RecursionTasks.LengthOfList(array.ToList()), Is.EqualTo(expectedCount));
+            Assert.That(RecursionTasks.LengthOfListRecursive(array.ToList()), Is.EqualTo(expectedCount));
         }
         
         [TestCase("А роза упала на лапу Азора.", true)]
@@ -34,6 +37,15 @@ namespace Recursion
         public void IsPalindromeRecursiveTest(string str, bool expected)
         {
             Assert.That(RecursionTasks.IsPalindrome(str), Is.EqualTo(expected));
+        }
+        
+        [TestCase("А роза упала на лапу Азора.", true)]
+        [TestCase("A man, a plan, a canal, Panama!", true)]
+        [TestCase("Сегодня отличная погода.", false)]
+        [TestCase("The sun is shining brightly.", false)]
+        public void IsPalindromeRecursiveWithLinkedListTest(string str, bool expected)
+        {
+            Assert.That(RecursionTasks.IsPalindromeWithLinkedList(str), Is.EqualTo(expected));
         }
         
         [TestCase(new int[] {1, 2, 3, 4, 5, 6}, new int[] {2, 4, 6})]
@@ -56,10 +68,84 @@ namespace Recursion
         [TestCase(new int[] {8, 8, 7, 5, 3, 1}, 8)]
         [TestCase(new int[] {1, 3, 5, 7, 8, 8}, 8)]
         [TestCase(new int[] {1, 2}, 1)]
-        [TestCase(new int[] {2,1}, 1)]
+        [TestCase(new int[] {2, 1}, 1)]
         public void FindSecondMaxTest(int[] dataForList, int expected)
         {
             Assert.That(RecursionTasks.FindSecondMax(dataForList.ToList()), Is.EqualTo(expected));
+        }
+
+        [Test]
+        public void FindAllFilesTest()
+        {
+            string testRootDirectory = Path.Combine(Path.GetTempPath(), "FileSearchTest");
+            
+            List<string> expectedFiles = CreateTestDirectoryStructure(testRootDirectory)
+                .Select(fi => fi.Name)
+                .OrderBy(name => name)
+                .ToList();
+            List<string> actualFiles = RecursionTasks.FindAllFiles(testRootDirectory)
+                .Select(fi => fi.Name)
+                .OrderBy(name => name)
+                .ToList();
+            
+            Assert.That(actualFiles, Is.EquivalentTo(expectedFiles));
+            
+            Directory.Delete(testRootDirectory, true);
+        }
+
+        [TestCase(2, new string[] {"()()", "(())"})]
+        [TestCase(3, new string[] {"((()))", "(()())", "(())()", "()(())", "()()()"})]
+        public void GenerateBalancedBrackets(int count, string[] expected)
+        {
+            Assert.That(RecursionTasks.GenerateBalancedBrackets(count), Is.EquivalentTo(expected));
+        }
+
+        private List<FileInfo> CreateTestDirectoryStructure(string rootDirectory)
+        {
+            List<FileInfo> result = new List<FileInfo>();
+            Directory.CreateDirectory(rootDirectory);
+
+            // Уровень 1 
+            result.Add(CreateTestTxtFile(rootDirectory, "root_file_1"));
+            result.Add(CreateTestTxtFile(rootDirectory, "root_file_2"));
+            
+            string level1Dir1 = Path.Combine(rootDirectory, "Level1_Dir1");
+            string level1Dir2 = Path.Combine(rootDirectory, "Level1_Dir2");
+            Directory.CreateDirectory(level1Dir1);
+            Directory.CreateDirectory(level1Dir2);
+
+            // Уровень 2
+            result.Add(CreateTestTxtFile(level1Dir1, "root_file_3"));
+            result.Add(CreateTestTxtFile(level1Dir1, "root_file_4"));
+            
+            result.Add(CreateTestTxtFile(level1Dir2, "root_file_5"));
+            result.Add(CreateTestTxtFile(level1Dir2, "root_file_6"));
+            
+            string level2Dir1 = Path.Combine(level1Dir1, "Level2_Dir1");
+            string level2Dir2 = Path.Combine(level1Dir1, "Level2_Dir2");
+            Directory.CreateDirectory(level2Dir1);
+            Directory.CreateDirectory(level2Dir2);
+            
+            string level2Dir3 = Path.Combine(level1Dir2, "Level2_Dir3");
+            string level2Dir4 = Path.Combine(level1Dir2, "Level2_Dir4");
+            Directory.CreateDirectory(level2Dir3);
+            Directory.CreateDirectory(level2Dir4);
+            
+            result.Add(CreateTestTxtFile(level2Dir1, "root_file_7"));
+            result.Add(CreateTestTxtFile(level2Dir2, "root_file_8"));
+            
+            result.Add(CreateTestTxtFile(level2Dir3, "root_file_9"));
+            result.Add(CreateTestTxtFile(level2Dir4, "root_file_10"));
+
+            return result;
+        }
+
+        private FileInfo CreateTestTxtFile(string directory, string fileName)
+        {
+            string filePath = Path.Combine(directory, $"{fileName}.txt");
+            File.WriteAllText(filePath, "");
+
+            return new FileInfo(filePath);
         }
     }
 }
